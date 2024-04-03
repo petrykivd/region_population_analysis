3e91477d09fe913c70074c4fa6a4fccad6463e5d
petrykivd <petrykiv.dmytro19@gmail.com>
2024-04-03 20:07:52 +0300

    Implemented DB connection

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+DB_HOST = os.environ.get("POSTGRES_HOST")
+DB_PORT = os.environ.get("POSTGRES_PORT")
+DB_NAME = os.environ.get("POSTGRES_DB")
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASS = os.environ.get("POSTGRES_PASSWORD")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
